6c55e58eed902a8ef959ac4ce3753c8135a3c9d6
Jang JunHee <lyradortiz913@naver.com>
2021-01-28 22:44:20 +0900

    split and insert

--- ch22/treap.cc
+++ ch22/treap.cc
@@ -20,3 +20,41 @@ struct Node {
         if(right) size += right ->size;
     }
 };
+
+//쪼개기 구현
+
+typedef pair<Node*, Node*> NodePair;
+//root를 루트로 하는 트립을 key미만의 값과 이상의 값을 갖는 
+//두 개의 트립으로 분리한다.
+NodePair split(Node*root, KeyType key){
+    if(root == nullptr) return NodePair(nullptr, nullptr);
+    //루트가 key미만이면 오른쪽 서브트립을 쪼갠다.
+    if(root->key < key){
+        NodePair rs = split(root->right, key);
+        root->setRight(rs.first);
+        return NodePair(root, rs.second);
+    }
+    //루트가 key 이상이면 왼쪽 서브트리를 쪼갠다.
+    NodePair ls = split(root->left, key);
+    root->setRight(ls.second);
+    return NodePair(ls.first, root);
+}
+
+//root를 루트로 하는 트립에 새 노드 node 를 삽입한 뒤 결과 트립의
+//루트를 반환한다.
+Node * insert(Node * root, Node * node){
+    if(root == nullptr) return node;
+    //node가 root를 대체해야한다. 해당 서브트립을
+    //반으로 잘라 각각의 자손으로 한다.
+    if(root->priority < node->priority){
+        NodePair splitted = split(root, node->key);
+        node->setLeft(splitted.first);
+        node->setRight(splitted.second);
+        return node;
+    }
+    else if(node->key < root -> key)
+        root->setLeft(insert(root->left, node));
+    else
+        root->setRight(insert(root->right, node));
+    return root;
+}
